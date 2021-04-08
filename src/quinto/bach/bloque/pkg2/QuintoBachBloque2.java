69bc0ba86a41616c3b8c9f7bb3911ac2865d33b1
Bryan García <antonygarcideleon@gmail.com>
2021-04-07 18:48:34 -0600

    Tarea 1

---
+++ src/quinto/bach/bloque/pkg2/QuintoBachBloque2.java
@@ -0,0 +1,8 @@
+package quinto.bach.bloque.pkg2;
+public class QuintoBachBloque2 {
+
+    public static void main(String[] args) {
+        System.out.println("Bryan Antony García de León");
+    }
+    
+}
